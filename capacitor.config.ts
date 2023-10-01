f22fb8d874f04b8f96aa03d7f3fc1385e86dded2
Mohammed Imran <mohammedimran15@gmail.com>
2023-10-01 18:38:30 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pretty-jewellers',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
